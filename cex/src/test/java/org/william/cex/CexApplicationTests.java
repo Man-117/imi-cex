b2281f214c0547fce9e1b68bf8fb4dd6f5b6c65b
Man-117 <seven.sun.work@qq.com>
2025-12-31 19:54:42 +0800

    Initial commit

---
+++ cex/src/test/java/org/william/cex/CexApplicationTests.java
@@ -0,0 +1,13 @@
+package org.william.cex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
